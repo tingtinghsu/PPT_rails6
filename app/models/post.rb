3f2cec7e6225e0acfb164e95261ebd299c408097
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-08 20:33:21 +0800

    rake serial, navbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum: 2}
+  validates :serial, presence: true, uniqueness: true, allow_nil: true
+
   belongs_to :board, optional: true
+
+  before_create :create_serial
+
+  private
+  def create_serial
+    self.serial = serial_generator(10)
+  end
+
+  def serial_generator(n)
+    [*'a'..'z', *'A'..'Z', *0..9].sample(n).join
+  end
 end
