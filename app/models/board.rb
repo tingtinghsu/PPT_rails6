3af87aca49fd07c7b8be3f9c0138dd0e95849124
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-08 16:32:39 +0800

    acts_as_paranoid with_deleted

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
+  acts_as_paranoid
+
   validates :title, :intro, presence: true, length: {minimum: 2}
-  def destroy
-    update(deleted_at: Time.now)
-  end
+
 end
