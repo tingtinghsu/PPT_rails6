8eb8c778b1ec8a50de9a13c147ce7b9a462760ff
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-08 16:03:46 +0800

    CRUD function finished

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
   def index
     @boards = Board.all
   end
@@ -17,11 +18,31 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to boards_path, notice: '更新成功！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path, notice: '刪除成功！'    
   end
 
   private
+  def find_board
+    @board = Board.find(params[:id])
+  end
+  # Strong Parameters
   def board_params
     params.require(:board).permit(:title, :intro)
   end
+
 end
